2e08e0bd1a9633d1d9d2cd619cf8accce2ccb6a8
razmi0 <121302828+razmi0@users.noreply.github.com>
2023-10-20 16:25:29 +0200

    feat : icons related data in new folder

---
+++ src/icons/index.ts
@@ -0,0 +1,49 @@
+type TypeData = {
+  [key: string]: {
+    color: string;
+  };
+};
+export const typeData: TypeData = {
+  bug: {
+    color: "#44b820",
+  },
+  electric: {
+    color: "#F8D030",
+  },
+  fire: {
+    color: "#F08030",
+  },
+  grass: {
+    color: "#78C850",
+  },
+  water: {
+    color: "#6890F0",
+  },
+  normal: {
+    color: "#A8A878",
+  },
+  poison: {
+    color: "#A040A0",
+  },
+  ground: {
+    color: "#E0C068",
+  },
+  flying: {
+    color: "#A890F0",
+  },
+  psychic: {
+    color: "#F85888",
+  },
+  ghost: {
+    color: "#705898",
+  },
+  rock: {
+    color: "#B8A038",
+  },
+  fairy: {
+    color: "#EE99AC",
+  },
+  fighting: {
+    color: "#C03028",
+  },
+};
